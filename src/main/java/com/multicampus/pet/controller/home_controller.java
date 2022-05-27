6dadeb1d49e39bcbb264d674253eecc4b1b9b5e5
Yooesuiseon <kgh4464@kakao.com>
2022-05-27 11:43:13 +0900

    sts4 option

---
+++ src/main/java/com/multicampus/pet/controller/home_controller.java
@@ -0,0 +1,10 @@
+package com.multicampus.pet.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class home_controller {
+	public String home() {
+		return "home";
+	}
+}
